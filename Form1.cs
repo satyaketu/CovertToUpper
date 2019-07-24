b7ca9aeebcffe78c13b68d62d9c1d16319ef0b9a
satyaketu <satyaketujoshi17@gmail.com>
2019-07-25 00:09:39 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertTOupper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string ConvertToUpper(string input)
+        {
+            string output = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] >= 'a' && input[i] <= 'z')
+                {
+                    output += (char)(input[i] - 'a' + 'A');
+                }
+                else
+                {
+                    output += input[i];
+                }
+            }
+            return output;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = ConvertToUpper(textBox1.Text);
+        }
+    }
+}
